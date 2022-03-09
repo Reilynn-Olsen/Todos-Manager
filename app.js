33f45cdf405a035bc4297fd12b7b3c6a7f59dfe8
Reilynn <reiolsen00@gmail.com>
2022-03-09 14:59:45 -0700

    routing

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRouter);
+app.get('/', (req, res) => res.sendFile('index.html'))
 
 module.exports = app;
